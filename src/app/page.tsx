5aab04c4839f5ab8f3f34df130a84297141a86b6
Dhuvii <thuvaraganuideisgns@gmail.com>
2024-06-10 18:03:37 +0530

    feat:Fixtures added.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ import {
 } from "react";
 import KandbanCard from "./components/KanbanCard";
 import { Key } from "./components/Key";
+import { applied, interview, phoneScreening, testimonials } from "./fixtures";
 
 export default function Home() {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -43,81 +44,6 @@ export default function Home() {
     }
     return null;
   };
-
-  const testimonials = [
-    {
-      name: "John Doe",
-      content:
-        "This service was fantastic! I couldn't be happier with the results.",
-      position: "CEO, Example Corp",
-      image:
-        "https://images.pexels.com/photos/886285/pexels-photo-886285.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    },
-    {
-      name: "Jane Smith",
-      content: "Amazing experience, highly recommend to everyone.",
-      position: "Marketing Manager, Tech Solutions",
-      image:
-        "https://images.pexels.com/photos/2448531/pexels-photo-2448531.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    },
-    {
-      name: "Robert Johnson",
-      content: "The team was professional and exceeded our expectations.",
-      position: "Product Manager, Innovate Inc.",
-      image:
-        "https://images.pexels.com/photos/10276340/pexels-photo-10276340.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    },
-    {
-      name: "Emily Davis",
-      content: "Top-notch service from start to finish. Will use again!",
-      position: "HR Specialist, People First",
-      image:
-        "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    },
-    {
-      name: "Michael Brown",
-      content: "Exceptional quality and great attention to detail.",
-      position: "Lead Developer, CodeWorks",
-      image:
-        "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    },
-    {
-      name: "Jessica Wilson",
-      content: "Friendly staff and excellent support throughout the project.",
-      position: "Project Coordinator, BuildRight",
-      image:
-        "https://images.pexels.com/photos/2918513/pexels-photo-2918513.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    },
-    {
-      name: "William Martinez",
-      content: "Delivered exactly what was promised, on time and on budget.",
-      position: "Operations Manager, Global Solutions",
-      image:
-        "https://images.pexels.com/photos/2340978/pexels-photo-2340978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    },
-    {
-      name: "Olivia Anderson",
-      content: "Very satisfied with the results. Will definitely return!",
-      position: "Creative Director, DesignPro",
-      image:
-        "https://images.pexels.com/photos/3772510/pexels-photo-3772510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    },
-    {
-      name: "David Thomas",
-      content: "Professional, efficient, and great communication.",
-      position: "Business Analyst, Future Insights",
-      image:
-        "https://images.pexels.com/photos/3778212/pexels-photo-3778212.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    },
-    {
-      name: "Sophia Garcia",
-      content:
-        "Absolutely wonderful! Could not have asked for a better experience.",
-      position: "Sales Executive, Market Leaders",
-      image:
-        "https://images.pexels.com/photos/3775156/pexels-photo-3775156.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    },
-  ];
 
   return (
     <main className="">
@@ -643,57 +569,6 @@ const ContainerWithPlusCorner = ({
     </div>
   );
 };
-
-const applied = [
-  {
-    name: "Amy Litchy",
-    date: "3 days ago",
-    image:
-      "https://images.pexels.com/photos/886285/pexels-photo-886285.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    match: 80,
-  },
-  {
-    name: "Doug Jacobs",
-    date: "2 days ago",
-    image:
-      "https://images.pexels.com/photos/1040881/pexels-photo-1040881.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    match: 65,
-  },
-  {
-    name: "Enrique Aguilar",
-    date: "5 days ago",
-    image:
-      "https://images.pexels.com/photos/1080213/pexels-photo-1080213.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    match: 25,
-  },
-];
-
-const phoneScreening = [
-  {
-    name: "Enrique Aguilar",
-    date: "5 days ago",
-    image:
-      "https://images.pexels.com/photos/1080213/pexels-photo-1080213.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    match: 15,
-  },
-  {
-    name: "Amy Litchy",
-    date: "3 days ago",
-    image:
-      "https://images.pexels.com/photos/886285/pexels-photo-886285.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    match: 80,
-  },
-];
-
-const interview = [
-  {
-    name: "Doug Jacobs",
-    date: "2 days ago",
-    image:
-      "https://images.pexels.com/photos/1040881/pexels-photo-1040881.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    match: 85,
-  },
-];
 
 const Kanban = () => {
   return (
